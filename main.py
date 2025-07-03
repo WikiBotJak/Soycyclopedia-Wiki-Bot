1ba96e06288da3e46a78ca67b4a80064057b27e0
WikiBotJak <contact@fenriris.net>
2025-07-02 19:43:52 -0700

    added an automaniac Daily Main Page Article Updater

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
-import time
-
 import pywikibot
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
-from infoboxUpdater import  InfoboxUpdater
+from scripts.infoboxUpdater import InfoboxUpdater
+from scripts.updateNewesetArticles import MainPageArticleUpdater
 
-def update_infoboxes():
+def login_bot():
     site = pywikibot.Site()
     if not site.logged_in():
         site.login()
     print(f"Logged in as: {site.user()}")
-    # run task 1 - Update post count for infobox soyjak templates
+    return site
+
+def update_infoboxes():
+    site = login_bot()
     updater = InfoboxUpdater(site)
+    updater.run()
+
+def update_na():
+    site = login_bot()
+    updater = MainPageArticleUpdater(site)
     updater.run()
 
 
 def main():
     scheduler = BlockingScheduler() #BlockingScheduler keeps the script running
+
+    # Run daily
+    scheduler.add_job(
+        update_na,
+        trigger=CronTrigger(hour=0, minute=0),
+        name="Daily Main Page Article Update",
+        misfire_grace_time=3600 # if missed, run within an hour
+    )
 
     # Run every Friday
     scheduler.add_job(
---
+++ scripts/updateNewesetArticles.py
@@ -0,0 +1,38 @@
+import pywikibot
+from datetime import datetime
+
+class MainPageArticleUpdater:
+    def __init__(self, site):
+        self.site = site
+        self.excluded_prefix = "Featured Gem:Main Page/Featured Gem"
+        self.page_title = "Main Page/NA"
+        self.max_new_pages = 5
+
+    def run(self):
+        new_pages = []
+        page = pywikibot.Page(self.site, self.page_title)
+
+
+        for change in self.site.recentchanges(namespaces=0, changetype='new', total=20):
+            title = change['title']
+            if title.startswith(self.excluded_prefix):
+                continue
+            new_pages.append(f"[[{title}]]")
+            if len(new_pages) == self.max_new_pages:
+                break
+
+        # Build post content
+        joined_links = " • ".join(new_pages)
+
+        post = f"""<div style="text-align: center;">
+            <div style="display: inline-block; text-align: left;">
+        {{{{Post|'''{joined_links}'''
+        
+        |Soot|Sun 20 Sep 2020 16:41:15|soy|1|700}}}}
+            </div>
+        </div>"""
+
+        # Update the wiki page
+        print(f"Updating newest mainspace articles ({joined_links})")
+        page.text = post
+        page.save(summary="Update newest mainspace articles")
